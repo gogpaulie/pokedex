728e4725628884e5e173108bab6784d8aa092ade
GoG_Paulie <ppolih21@gmail.com>
2020-07-08 21:05:09 -0400

    Updates

---
+++ precache-manifest.c4315fddc874bae8dfa0928828bc8211.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec606cd3033d9a31041f18e62c858f7b",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "ef5544edeacabbb3e494",
+    "url": "/pokedex/static/css/main.3bf542ab.chunk.css"
+  },
+  {
+    "revision": "15584cc71568c2c0a1a2",
+    "url": "/pokedex/static/js/2.cf1dfe39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.cf1dfe39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef5544edeacabbb3e494",
+    "url": "/pokedex/static/js/main.b44e4f79.chunk.js"
+  },
+  {
+    "revision": "5d5b7317cf43a0b8925c",
+    "url": "/pokedex/static/js/runtime-main.2d94522c.js"
+  }
+]);
